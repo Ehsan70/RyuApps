fb433be93dd30a2157ba2f1c9580f5a122d0bb0c
Ehsan70 <a.ehsan70@gmail.com>
2015-06-29 08:35:27 -0700

    Added the l2 file

---
+++ l2.py
@@ -0,0 +1,11 @@
+__author__ = 'Ehsan'
+
+from ryu.base import app_manager
+
+class L2Switch(app_manager.RyuApp):
+    def __init__(self, *args, **kwargs):
+        super(L2Switch, self).__init__(*args, **kwargs)
+
+
+
+
