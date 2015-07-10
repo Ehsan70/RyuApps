6a3becab031af0c23e4789f93fbe42782f30006a
Ehsan70 <a.ehsan70@gmail.com>
2015-07-10 13:45:47 -0700

    Noting serous

--- Pkt_Topo_with_loop.py
+++ Pkt_Topo_with_loop.py
@@ -1,25 +1,27 @@
 __author__ = 'Ehsan'
-
+from mininet.node import CPULimitedHost
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.log import setLogLevel, info
 from mininet.node import RemoteController
 from mininet.cli import CLI
-
+from mininet.link import TCLink
 """
 Instructions to run the topo:
     1. Go to directory where this fil is.
     2. run: sudo -E python Pkt_Topo_with_loop.py
 """
 
+
 class Simple3PktSwitch(Topo):
     """Simple topology example."""
 
-    def __init__(self):
+    def __init__(self, **opts):
         """Create custom topo."""
 
         # Initialize topology
-        Topo.__init__(self)
+        super(Simple3PktSwitch, self).__init__(**opts)
+        #Topo.__init__(self)
 
         # Add hosts and switches
         h1 = self.addHost('h1')
@@ -27,27 +29,38 @@ class Simple3PktSwitch(Topo):
         h3 = self.addHost('h3')
         h4 = self.addHost('h4')
 
+        opts = dict(protocols='OpenFlow13')
+
         # Adding switches
-        p1 = self.addSwitch('p1', dpid="0000000000000001")
-        p2 = self.addSwitch('p2', dpid="0000000000000002")
-        p3 = self.addSwitch('p3', dpid="0000000000000003")
-        p4 = self.addSwitch('p4', dpid="0000000000000004")
+        s1 = self.addSwitch('s1', dpid="0000000000000001")
+        s2 = self.addSwitch('s2', dpid="0000000000000002")
+        s3 = self.addSwitch('s3', dpid="0000000000000003")
+        s4 = self.addSwitch('s4', dpid="0000000000000004")
 
         # Add links
-        self.addLink(h1, p1)
-        self.addLink(h2, p2)
-        self.addLink(h3, p3)
-        self.addLink(h4, p4)
+        self.addLink(h1, s1)
+        self.addLink(h2, s2)
+        self.addLink(h3, s3)
+        self.addLink(h4, s4)
 
-        self.addLink(p2, p4)
-        self.addLink(p1, p2)
-        self.addLink(p3, p4)
-        self.addLink(p1, p3)
+        self.addLink(s2, s4)
+        self.addLink(s2, s3)
+        self.addLink(s1, s2)
+        self.addLink(s3, s4)
+        self.addLink(s1, s3)
+
+
+def installStaticFlows(net):
+    for sw in net.switches:
+        info('Adding flows to %s...' % sw.name)
+        sw.dpctl('add-flow', 'in_port=1,actions=output=2')
+        sw.dpctl('add-flow', 'in_port=2,actions=output=1')
+        info(sw.dpctl('dump-flows'))
 
 
 def run():
     c = RemoteController('c', '0.0.0.0', 6633)
-    net = Mininet(topo=Simple3PktSwitch(), controller=None, autoSetMacs=True)
+    net = Mininet(topo=Simple3PktSwitch(), host=CPULimitedHost, controller=None)
     net.addController(c)
     net.start()
 
